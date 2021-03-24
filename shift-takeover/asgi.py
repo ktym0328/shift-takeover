28f88db114997596be2908b22e400e4858b2a63c
ktym0328 <ktym.ttsuy.a6@gmail.com>
2021-03-24 21:54:18 +0900

    一部保存漏れ

--- shift-takeover/asgi.py
+++ shift-takeover/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shift-takeover.settings')
 
 application = get_asgi_application()
